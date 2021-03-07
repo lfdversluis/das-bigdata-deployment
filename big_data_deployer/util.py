a429b4a35360e20f9bf00454a51c4c0ce9fb7452
LV <lfdversluis@gmail.com>
2021-03-07 13:43:50 +0100

    Remove shell

--- big_data_deployer/util.py
+++ big_data_deployer/util.py
@@ -21,8 +21,8 @@ def create_log_fn(base_indentation, base_log=log):
 def execute_command_quietly(command_line_list):
     """Executes a command, given as a list, while supressing any output."""
     with open(os.devnull, "wb") as devnull:
-        subprocess.check_call(command_line_list, stdout=devnull, stderr=subprocess.STDOUT, shell=True)
+        subprocess.check_call(command_line_list, stdout=devnull, stderr=subprocess.STDOUT)
 
 def execute_command_for_output(command_line_list):
-    return subprocess.Popen(command_line_list, stdout=subprocess.PIPE, shell=True).communicate()[0].decode("utf-8")
+    return subprocess.Popen(command_line_list, stdout=subprocess.PIPE).communicate()[0].decode("utf-8")
 
